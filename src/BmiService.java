0bb9f640076d63a5ffb561f105c8302317089abe
indigonext <novikovandrey7177@gmail.com>
2022-05-27 12:33:12 +0300

    Первый коммит

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public double calculate(double mass){
+        double height = 1.82;
+                double bmi = (mass / (height * height));
+                return bmi;
+    }
+}
